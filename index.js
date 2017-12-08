51ff8cb6a1682a2fa5046a38bca647d91dce76cc
Bran van der Meer <branmovic@gmail.com>
2017-12-08 14:11:24 +0100

    feat: An existing mapper file is now optional

--- index.js
+++ index.js
@@ -5,47 +5,72 @@ var path = require('path')
 var existsSync = require('fs').existsSync
 var scripts = require('scripts')
 
-/**
- * @param {String} mapperFile
- * @returns {ElectrolyteAssemblyMapper}
- */
-module.exports = function ElectrolyteAssemblyMapperFactory (mapperFile) {
-  var mapping
+module.exports = factory
 
-  try {
-    var mapperFilePath = path.resolve(mapperFile)
+function factory (mapperFile) {
+  var mapperFilePath = path.resolve(mapperFile)
+  if (existsSync(mapperFilePath)) {
     debug('Loading mapper file:', mapperFilePath)
-    mapping = require(mapperFilePath)
-  } catch (e) {
-    throw new Error('electrolyte-assembly-mapper: Could not find supplied mapper file: ' + mapperFile)
+    var mapping = require(mapperFilePath)
+    return createMapperLoader(mapping)
   }
+  debug('Not using supplied mapper file: ' + mapperFile)
+  return dirLoader
+}
 
-  return function ElectrolyteAssemblyMapper (/* [dir, ...] */) {
-    var dirs = [].slice.call(arguments)
-    if (dirs.length < 1) {
-      throw new Error('electrolyte-assembly-mapper: No directories supplied!')
-    }
+function dirLoader (/* [dirs, ...] */) {
+  var dirs = destructureArgs(arguments)
+  return function (id) {
+    return loadAll(dirs, id)
+  }
+}
 
-    return function ElectrolyteAssemblyMapperLoader (id) {
-      for (var dirIdx in dirs) {
-        if (!dirs.hasOwnProperty(dirIdx)) {
-          continue
-        }
-        var dir = dirs[dirIdx]
-
-        if (mapping[id]) {
-          debug('Mapping dependency:', id, 'to:', mapping[id])
-          id = mapping[id]
-        }
-
-        const file = path.join(path.resolve(dir), id)
-        const filepath = scripts.resolve(file)
-
-        if (existsSync(filepath)) {
-          debug('Loading dependency:', id, 'from disk:', filepath)
-          return require(filepath)
-        }
-      }
+function createMapperLoader (mapping) {
+  return function mapperLoader (/* [dirs, ...] */) {
+    var dirs = destructureArgs(arguments)
+    return function (id) {
+      return loadAll(dirs, map(mapping, id))
     }
   }
+}
+
+function loadAll (dirs, id) {
+  for (var i = 0, len = dirs.length; i < len; i++) {
+    if (!dirs.hasOwnProperty(i)) {
+      continue
+    }
+    var dir = dirs[i]
+
+    var mod = loadOne(dir, id)
+    if (mod) {
+      return mod
+    }
+  }
+}
+
+function loadOne (dir, id) {
+  var file = path.join(path.resolve(dir), id)
+  var filepath = scripts.resolve(file)
+
+  if (existsSync(filepath)) {
+    debug('Loading dependency:', id, 'from disk:', filepath)
+    return require(filepath)
+  }
+  debug('Could not find dependency:', id, 'on disk:', filepath)
+}
+
+function destructureArgs (args) {
+  var dirs = [].slice.call(args)
+  if (dirs.length < 1) {
+    throw new Error('electrolyte-assembly-mapper: No directories supplied!')
+  }
+  return dirs
+}
+
+function map (mapping, id) {
+  if (mapping[id]) {
+    debug('Mapping dependency:', id, 'to:', mapping[id])
+    return mapping[id]
+  }
+  return id
 }
